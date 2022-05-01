aabcd44f90f8c12645572c49bf4f87e2ae86d636
codewizz <c.chaiwat.ch@gmail.com>
2022-05-01 10:57:24 +0700

    update oauth2 create access token

--- utils/oauth2.py
+++ utils/oauth2.py
@@ -1,4 +1,5 @@
 from datetime import timedelta, datetime
+import tkinter
 from typing import Optional
 
 from fastapi import Depends, HTTPException, status
@@ -6,3 +7,22 @@ from fastapi.security import OAuth2PasswordBearer
 
 from jose import JWTError, jwt
 from decouple import config
+
+
+SECRET_KEY = config("SECRET_KEY")
+ALGORITHM = config("ALGORITHM")
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def create_access_token(data: dict, expire_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = 
+
+
+def generate_expire_date(expire_delta: Optional[timedelta] = None):
+    if expire_delta:
+        expire = datetime.utcnow() + expire_delta
+    else:
+        expire = datetime.utcnow() + timedelta(days=1)
+    return expire
