561ab9b2ad28a49deba2e0957ebd3b6094768351
codewizz <c.chaiwat.ch@gmail.com>
2022-05-01 11:08:43 +0700

    update token router

--- routers/auth/authen_router.py
+++ routers/auth/authen_router.py
@@ -1,3 +1,4 @@
+from os import stat
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 
@@ -14,5 +15,27 @@ router = APIRouter(tags=["authentication"])
 
 
 @router.post("/token")
-def login():
-    return {"hello": "token"}
+def login(
+    request: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    user = db.query(DbUser).filter(DbUser.username == request.username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
+    elif not Hash.verify(user.password, request.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect password"
+        )
+    else:
+        access_token = create_access_token(data={"sub": user.username})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_id": user.id,
+        "username": user.username
+    }
