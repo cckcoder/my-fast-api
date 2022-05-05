c36dc8c7eeb997320186a1a07f96b01a8b94bfa2
codewizz <c.chaiwat.ch@gmail.com>
2022-05-05 22:25:14 +0700

    config database and profile

--- models/database.py
+++ models/database.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI
+from email.policy import default
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from decouple import config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./my-fast-api.db"
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if DEBUG:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./my-fast-api.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    DATABASE_USER = config("DATABASE_USER")
+    DATABASE_PASSWORD = config("DATABASE_PASSWORD ")
+    DATABASE_NAME = config("DATABASE_NAME ")
+    DATABASE_HOST = config("DATABASE_HOST ")
+    SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
 
 seesion_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
