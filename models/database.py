1989666c52c54a570cf2318c42bbc5d97b08c3ef
codewizz <c.chaiwat.ch@gmail.com>
2022-05-05 22:34:00 +0700

    create engine for create db

--- models/database.py
+++ models/database.py
@@ -19,6 +19,7 @@ else:
     DATABASE_NAME = config("DATABASE_NAME")
     DATABASE_HOST = config("DATABASE_HOST")
     SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 seesion_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
